13cedb42bd2d0d506ffab87e06ebf963ed939a6a
Dawid Skowronski <d.skowronski.dev@gmail.com>
2023-09-22 15:18:09 +0200

    improved validation on image views

--- app/image_access/views.py
+++ app/image_access/views.py
@@ -8,12 +8,20 @@ from mimetypes import guess_type
 def resized_image_view(request, filename):
     slug = filename.split('.')[0]
     image = get_object_or_404(ResizedImage, slug=slug)
+
+    if image.height not in image.original.uploader.tier.thumbnailHeights:
+        return HttpResponseForbidden()
+
     return HttpResponse(image.resized_file.read(), content_type=guess_type(image.resized_file.name)[0])
 
 
 def original_image_view(request, filename):
     slug = filename.split('.')[0]
-    image = Image.objects.select_related('uploader__tier').get(slug=slug)
+    try:
+        image = Image.objects.select_related('uploader__tier').get(slug=slug)
+    except (ObjectDoesNotExist):
+        return HttpResponseNotFound()
+
     if image.uploader.tier.original_link_access:
         return HttpResponse(image.file.read(), content_type=guess_type(image.file.name)[0])
     else:
